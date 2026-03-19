17833fe88577439159f54cd6df9c8e37a74b523b
coolguy1239837461 <114636547+coolguy1239837461@users.noreply.github.com>
2026-03-19 18:30:01 -0400

    Create uv.sw.js

---
+++ public/uv/uv.sw.js
@@ -0,0 +1,10 @@
+/* Reference the bundle from the same folder */
+importScripts('./uv.bundle.js');
+importScripts('./uv.config.js');
+importScripts(__uv$config.sw || './uv.sw.js');
+
+const sw = new UVServiceWorker();
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(sw.fetch(event));
+});
