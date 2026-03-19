3d8c4d705fa2cf5aaf54caa35b178c3a14f27cfc
coolguy1239837461 <114636547+coolguy1239837461@users.noreply.github.com>
2026-03-19 17:29:46 -0400

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,7 @@
-// This file overwrites the stock UV config.js
-
 self.__uv$config = {
   prefix: "/uv/service/",
+  /* This connects your Netlify frontend to your Pi */
+  bare: "https://raspiultraviolet.share.zrok.io/bare/", 
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
